7f37a78417ef44c45fedb72e1cccab841bfe952b
Adiba Haque <adibahaque@umass.edu>
2023-04-29 16:26:10 -0400

    add custom user model

--- Backend/elena_project/elena/models.py
+++ Backend/elena_project/elena/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
 
@@ -8,3 +10,8 @@ class Location(models.Model):
 
     def __str__(self):
         return f"({self.lat}, {self.lon})"
+
+
+#use django's built-in Abstract User that has necessary fields such as username, email, password
+class CustomUser(AbstractUser):
+    pass
--- Backend/elena_project/elena_project/settings.py
+++ Backend/elena_project/elena_project/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'elena.CustomUser'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/

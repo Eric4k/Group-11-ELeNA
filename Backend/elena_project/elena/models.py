7e0f0846e97a7d8c4ca76141edd7173c034fe6c8
Adiba Haque <adibahaque@umass.edu>
2023-04-29 16:09:02 -0400

    updated models

--- Backend/elena_project/elena/models.py
+++ Backend/elena_project/elena/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+    def __str__(self):
+        return f"({self.lat}, {self.lon})"

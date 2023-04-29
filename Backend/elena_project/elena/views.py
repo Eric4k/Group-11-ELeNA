58e282b66e096689f2ea46cdc06c74c063b7937b
Adiba Haque <adibahaque@umass.edu>
2023-04-29 15:58:37 -0400

    option for elevation pref, mode of transport

--- Backend/elena_project/elena/views.py
+++ Backend/elena_project/elena/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+import osmnx as ox
+import requests
+
 
 # Create your views here.
 @api_view(["GET"])
@@ -12,6 +15,25 @@ def getRoute(request):
         startLatitude = request.GET.get('startLat', None)
         endLongitude = request.GET.get('endLong', None)
         endLatitude = request.GET.get('endLat', None)
+
+        #get the mode of transportation from the request parameters
+        modeOfTransport = request.GET.get('modeOfTransport', 'walk') #walk is the default mode
+
+        #get the user's elevation preference from the request parameters
+        elev_preference = request.GET.get('elev_preference', 'min') #min is the default choice
+
+        # Call OSMnx to get the nearest edges between the points
+        start_point = (float(startLatitude), float(startLongitude))
+        end_point = (float(endLatitude), float(endLongitude))
+        G = ox.graph_from_point(start_point, distance=1000, network_type=modeOfTransport)
+        start_node = ox.get_nearest_node(G, start_point)
+        end_node = ox.get_nearest_node(G, end_point)
+        # if elev_preference == 'min':
+        #     do something here
+        # elif elev_preference == 'max':
+        #     do something here
+
+
         if startLongitude == None or startLatitude == None or endLongitude == None or endLatitude == None:
             return Response({"status:" "Invalid Request"}, status=status.HTTP_400_BAD_REQUEST)
         else:  

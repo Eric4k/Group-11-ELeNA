99232d6d0980b5c86a98f52fafcea38931fe4fb4
Eric Huang <ejhuang@umass.edu>
2023-05-01 00:03:45 -0400

    add api call to osmnx to get graph

--- Backend/elena_project/elena/apps.py
+++ Backend/elena_project/elena/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from geoDataRetriever import initializeGeoDataGraphs
 
 class ElenaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'elena'
+    def ready(self):
+        #initialize the graphs when the server is started
+        initializeGeoDataGraphs()
--- Backend/elena_project/elena/geoDataRetriever.py
+++ Backend/elena_project/elena/geoDataRetriever.py
@@ -1,1 +1,16 @@
 import osmnx as ox
+
+def initializeGeoDataGraphs(self):
+    global geoDataGraphWalk
+    global geoDataGraphBike
+    global geoDataGraphDrive
+    
+    #TODO add elevation processing
+    geoDataGraphWalk = ox.graph_from_address("Amherst, MA", network_type='walk', simplify=False)
+    geoDataGraphWalk  = ox.add_edge_travel_times(ox.add_edge_speeds(geoDataGraphWalk))
+        
+    geoDataGraphBike = ox.graph_from_address("Amherst, MA", network_type='bike', simplify=False)
+    geoDataGraphBike  = ox.add_edge_travel_times(ox.add_edge_speeds(geoDataGraphBike))
+        
+    geoDataGraphDrive = ox.graph_from_address("Amherst, MA", network_type='drive', simplify=False)
+    geoDataGraphDrive  = ox.add_edge_travel_times(ox.add_edge_speeds(geoDataGraphDrive))

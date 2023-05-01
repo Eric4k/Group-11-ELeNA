c569d38c1a6e93de2413bbc08737b20d46eabef1
Eric Huang <ejhuang@umass.edu>
2023-05-01 19:42:59 -0400

    load the dataset

--- Backend/elena_project/elena/geoDataRetriever.py
+++ Backend/elena_project/elena/geoDataRetriever.py
@@ -1,40 +1,41 @@
 import osmnx as ox
 
-def loadGraphMLData(self):
+def loadGraphMLData():
     global geoDataGraphWalk
     global geoDataGraphBike
     global geoDataGraphDrive
     
-    geoDataGraphWalk = ox.load_graphml()
-    geoDataGraphBike = ox.load_graphml()
-    geoDataGraphDrive = ox.load_graphml()
+    geoDataGraphWalk = ox.load_graphml("dataSets/Amherst_Walk_Data.graphml")
+    geoDataGraphBike = ox.load_graphml("dataSets/Amherst_Bike_Data.graphml")
+    geoDataGraphDrive = ox.load_graphml("dataSets/Amherst_Drive_Data.graphml")
+
 
 # run once to create the graphs
-def initializeGeoDataGraphs(self):
+def initializeGeoDataGraphs():
     
     #retreive the networkx graph with direct edges
     walkGraph = ox.graph_from_address("Amherst, MA", network_type='walk', simplify=False)
     #add edge travel time
-    walkGraph = ox.add_edge_travel_times(ox.add_edge_speeds(geoDataGraphWalk))
+    walkGraph = ox.add_edge_travel_times(ox.add_edge_speeds(walkGraph))
     #add elevation to nodes
     walkGraph = ox.add_node_elevations_google(walkGraph, None, max_locations_per_batch=100, pause_duration=2, precision=3, url_template='https://api.opentopodata.org/v1/aster30m?locations={}&key={}')
     #save to graphml file
-    ox.save_graphml(walkGraph,"Backend/elena_project/elena/dataSets/Amherst_Walk_Data.graphml", gephi=False, encoding='utf-8')
+    ox.save_graphml(walkGraph,"dataSets/Amherst_Walk_Data.graphml", gephi=False, encoding='utf-8')
     
     #retreive the networkx graph with direct edges
     bikeGraph = ox.graph_from_address("Amherst, MA", network_type='bike', simplify=False)
     #add edge travel time
-    bikeGraph = ox.add_edge_travel_times(ox.add_edge_speeds(geoDataGraphBike))
+    bikeGraph = ox.add_edge_travel_times(ox.add_edge_speeds(bikeGraph))
     #add elevation to nodes
     bikeGraph = ox.add_node_elevations_google(bikeGraph, None, max_locations_per_batch=100, pause_duration=2, precision=3, url_template='https://api.opentopodata.org/v1/aster30m?locations={}&key={}')
     #save to graphml file
-    ox.save_graphml(bikeGraph,"Backend/elena_project/elena/dataSets/Amherst_Bike_Data.graphml", gephi=False, encoding='utf-8')
+    ox.save_graphml(bikeGraph,"dataSets/Amherst_Bike_Data.graphml", gephi=False, encoding='utf-8')
         
     #retreive the networkx graph with direct edges
     driveGraph = ox.graph_from_address("Amherst, MA", network_type='drive', simplify=False)
     #add edge travel time
-    driveGraph = ox.add_edge_travel_times(ox.add_edge_speeds(geoDataGraphDrive))
+    driveGraph = ox.add_edge_travel_times(ox.add_edge_speeds(driveGraph))
     #add elevation to nodes
     driveGraph = ox.add_node_elevations_google(driveGraph, None, max_locations_per_batch=100, pause_duration=2, precision=3, url_template='https://api.opentopodata.org/v1/aster30m?locations={}&key={}')
     #save to graphml file
-    ox.save_graphml(driveGraph,"Backend/elena_project/elena/dataSets/Amherst_Drive_Data.graphml", gephi=False, encoding='utf-8')
+    ox.save_graphml(driveGraph,"dataSets/Amherst_Drive_Data.graphml", gephi=False, encoding='utf-8')
--- Backend/elena_project/elena_project/settings.py
+++ Backend/elena_project/elena_project/settings.py
@@ -39,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'elena',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]

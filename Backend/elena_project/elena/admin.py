000862baad57eb25628678c5c66daf0744e700ae
Adiba Haque <adibahaque@umass.edu>
2023-05-12 00:06:35 -0400

    modification

--- Backend/elena_project/elena/admin.py
+++ Backend/elena_project/elena/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Location
 from .models import CustomUser
 
-# Register your models here.
+# register your models here.
 admin.site.register(Location)
 admin.site.register(CustomUser)
 
